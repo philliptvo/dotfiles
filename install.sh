4dc18c7e2a2372ce48b4a30ea481208cc9afdd61
Phillip Vo <phillip.t.vo@gmail.com>
2018-12-19 00:35:15 -0500

    Change to symlink config files by default. Copying can be done optionally by adding ".cp" token

--- install.sh
+++ install.sh
@@ -2,30 +2,45 @@
 
 set -ex
 
-# Copy config files in subdirectories
-cp_configs() {
-  for config in ${1}.[^.]*; do
-    targetfile="${HOME}/$(basename ${config})"
-
-    # clean up existing links
-    [ -f $targetfile ] && rm $targetfile
-    cp $config $targetfile
+# sym link config files in subdirectories
+symlink() {
+  for config in ${1}*; do
+    if [ -f $config ]; then
+      targetfile="${HOME}/.$(basename ${config})"
+      # clean up existing links
+      [ -L $targetfile ] && rm $targetfile
+      ln -s $config $targetfile
+    fi
   done
 }
 
-set_up() {
+cp_config() {
+  for config in ${1}*; do
+    if [ -f $config ]; then
+      targetfile="${HOME}/.$(basename ${config})"
+      # clean up existing links
+      [ -f $targetfile ] && rm $targetfile
+      cp $config $targetfile
+    fi
+  done
+}
+
+main() {
   for file in */;  do
     # copy to home directory if force token exists
-    if [ -f ${file}force ]; then
-    # clean up directory if it already exists
-    targetpath="${HOME}/.${file}"
-    [ -d $targetpath ] && rm -rf $targetpath
-
-    cp -r $file $targetpath
+    if [ -f ${file}.force ]; then
+      # clean up directory if it already exists
+      targetpath="${HOME}/.${file}"
+      [ -d $targetpath ] && rm -rf $targetpath
+      cp -r $file $targetpath
     fi
 
-    cp_configs $file
+    if [ -f ${file}.cp ]; then
+      cp_config $file
+    else
+      symlink $file
+    fi
   done
 }
 
-set_up
+main
