a1ac04cb34ba1cd76ea6d12e4077637e272d6fa1
Phillip Vo <phillip.t.vo@gmail.com>
2020-10-08 15:52:33 -0700

    Added local "bin" directory for local python development

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,7 @@
 if status --is-interactive
   set theme_color_scheme gruvbox
-  set -g fish_user_paths "/usr/local/texlive/2020/bin/x86_64-darwin" $fish_user_paths
+  #set -g fish_user_paths "/usr/local/texlive/2020/bin/x86_64-darwin" $fish_user_paths
+  set -gx PATH "/usr/local/texlive/2020/bin/x86_64-darwin" "$HOME/.local/bin" $PATH
+
+  pyenv init - | source
 end
