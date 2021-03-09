666a72b5c605a8673a47798b91e07e039779923b
Phillip Vo <phillip.t.vo@gmail.com>
2021-03-09 14:15:11 -0800

    Added env variables to fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,10 @@
 if status --is-interactive
   set theme_color_scheme gruvbox
   #set -g fish_user_paths "/usr/local/texlive/2020/bin/x86_64-darwin" $fish_user_paths
-  set -gx PATH "/usr/local/texlive/2020/bin/x86_64-darwin" "$HOME/.local/bin" $PATH
+  set -xg fish_user_paths "/usr/local/texlive/2020/bin/x86_64-darwin" "$HOME/.scripts" "$HOME/.local/bin" $fish_user_paths
+  set -xg EDITOR "nvim"
+  set -xg BROWSER ""
+  set -xg NOTES_DIR "$HOME/notes"
 
   pyenv init - | source
 end
